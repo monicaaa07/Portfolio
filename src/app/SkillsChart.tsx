b344616b7a39a9af4701187f97e6fdd85897c08a
Monica Anthony <monicaanthony@Monicas-MacBook-Air.local>
2023-12-21 12:18:36 -0800

    fix build error

--- src/app/SkillsChart.tsx
+++ src/app/SkillsChart.tsx
@@ -6,7 +6,8 @@ const SkillsChart: React.FC = () => {
 
   useEffect(() => {
     // D3 code to create a basic bar chart
-    const data: number[] = [5,6,8,7,6,6,6,9,2,2,1,2,6,10];
+    const numericalData: number[] = [5, 6, 8, 7, 6, 6, 6, 9, 2, 2, 1, 2, 6, 10];
+    const labelData = ["D3.js", "AmCharts", "Tableau", "Python", "Typescript", "HTML", "CSS", "SQL", "SSRS", "SSIS", "SSAS", "ML", "Node.js", "Linux"];
     // Select the existing chart and remove its contents
     const existingChart = d3.select('#bar-chart');
     existingChart.selectAll('*').remove();
@@ -16,11 +17,11 @@ const SkillsChart: React.FC = () => {
     const barPadding = 10;
 
     // Create a color scale based on the values
-    const colorScale = d3.scaleLinear<number>().domain([0, d3.max(data)!]).range(['#ccece6', '#006d2c']);
+    const colorScale = d3.scaleLinear<string>().domain([0, d3.max(numericalData)!]).range(['#ccece6', '#006d2c']);
 
     svg
       .selectAll('rect')
-      .data(data)
+      .data(numericalData)
       .enter()
       .append('rect')
       .attr('x', (d, i) => i * (barWidth + barPadding))
@@ -28,42 +29,37 @@ const SkillsChart: React.FC = () => {
       .attr('width', barWidth)
       .attr('height', (d) => 40)
       .attr('fill', (d) => colorScale(d));
-      
 
-
-       // Add labels to the center of each bar
+    // Add labels to the center of each bar
     svg
-    .selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
-    .text((d) => d)
-    .attr('x', (d, i) => i * (barWidth + barPadding) + barWidth / 2)
-    .attr('y', (d) => 400 - 40 / 2)
-    .attr('text-anchor', 'middle') // Center the text horizontally
-    .attr('alignment-baseline', 'middle') // Center the text vertically
-    .attr('fill', 'white')
+      .selectAll('text')
+      .data(numericalData)
+      .enter()
+      .append('text')
+      .text((d) => d)
+      .attr('x', (d, i) => i * (barWidth + barPadding) + barWidth / 2)
+      .attr('y', (d) => 400 - 40 / 2)
+      .attr('text-anchor', 'middle') // Center the text horizontally
+      .attr('alignment-baseline', 'middle') // Center the text vertically
+      .attr('fill', 'white');
 
-
-    const labelData = ["D3.js","AmCharts","Tableau","Python","Typescript","HTML","CSS","SQL","SSRS","SSIS","SSAS","ML","Node.js","Linux"]
     // Add x-axis labels
     svg
-    .selectAll('.x-axis-label')
-    .data(labelData)
-    .enter()
-    .append('text')
-    .text((d) => d)
-    .attr('x', (d, i) => i * (barWidth + barPadding) + barWidth / 3)
-    .attr('y', 400 + 30) // Adjust y to position below the x-axis
-    .attr('text-anchor', 'middle') // Center the text horizontally
-    .attr('alignment-baseline', 'middle') // Center the text vertically
-    .attr('fill', 'white')
-    .attr('class', 'x-axis-label')
-    .attr('transform', (d, i) => `rotate(-45, ${i * (barWidth + barPadding) + barWidth / 2}, ${400 + 20})`) // Rotate each label individually
-    .style('font-size', 12); 
-    
+      .selectAll('.x-axis-label')
+      .data(labelData)
+      .enter()
+      .append('text')
+      .text((d) => d)
+      .attr('x', (d, i) => i * (barWidth + barPadding) + barWidth / 3)
+      .attr('y', 400 + 30) // Adjust y to position below the x-axis
+      .attr('text-anchor', 'middle') // Center the text horizontally
+      .attr('alignment-baseline', 'middle') // Center the text vertically
+      .attr('fill', 'white')
+      .attr('class', 'x-axis-label')
+      .attr('transform', (d, i) => `rotate(-45, ${i * (barWidth + barPadding) + barWidth / 2}, ${400 + 20})`) // Rotate each label individually
+      .style('font-size', 12);
 
-}, []);
+  }, []);
 
   return <div id="bar-chart" style={{marginTop:'-25%',marginLeft:'15%'}}></div>;
 };
